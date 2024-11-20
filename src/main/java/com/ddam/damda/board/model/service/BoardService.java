a3efd53e3de6ddecf5a45950be88d28e79d9a745
2Ludy <dkdl8412@gmail.com>
2024-11-20 12:54:04 +0900

    BoardImage 기능 구현 완료

--- src/main/java/com/ddam/damda/board/model/service/BoardService.java
+++ src/main/java/com/ddam/damda/board/model/service/BoardService.java
@@ -1,5 +1,10 @@
 package com.ddam.damda.board.model.service;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
 import com.ddam.damda.board.model.Board;
 import com.ddam.damda.common.util.PageRequest;
 import com.github.pagehelper.PageInfo;
@@ -10,11 +15,11 @@ public interface BoardService {
 	
 	Board selectBoard(int id);
 	
-	int insertBoard(Board board);
+	int insertBoard(Board board, List<MultipartFile> images) throws IOException;
 	
-	int deleteBoard(int id);
+	int deleteBoard(int id) throws IOException;
 	
-	int updateBoard(Board board);
+	int updateBoard(Board board, List<MultipartFile> newImages, List<Integer> deleteImageIds) throws IOException;
 	
 	int updateViewCount(int id);
 	
--- src/main/java/com/ddam/damda/board/model/service/BoardServiceImpl.java
+++ src/main/java/com/ddam/damda/board/model/service/BoardServiceImpl.java
@@ -1,10 +1,13 @@
 package com.ddam.damda.board.model.service;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.ddam.damda.board.model.Board;
 import com.ddam.damda.board.model.mapper.BoardMapper;
@@ -35,26 +38,75 @@ public class BoardServiceImpl implements BoardService {
 	@Transactional
 	@Override
 	public Board selectBoard(int id) {
-		return boardMapper.selectBoard(id);
+		Board board = boardMapper.selectBoard(id);
+		if (board != null) {
+            board.setImages(boardImageService.findByBoardId(id));
+        }
+		return board;
 	}
 
 	@Transactional
 	@Override
-	public int insertBoard(Board board) {
-		return boardMapper.insertBoard(board);
-	}
+	public int insertBoard(Board board, List<MultipartFile> images) throws IOException {
+        // 1. 게시글 저장
+        int result = boardMapper.insertBoard(board);
+        if (result <= 0) {
+            throw new RuntimeException("게시글 저장 실패");
+        }
+
+        // 2. 이미지 처리
+        if (images != null && !images.isEmpty()) {
+            List<BoardImage> savedImages = new ArrayList<>();
+            for (MultipartFile image : images) {
+                if (!image.isEmpty()) {
+                    BoardImage savedImage = boardImageService.saveBoardImage(image, board.getId());
+                    savedImages.add(savedImage);
+                }
+            }
+            board.setImages(savedImages);
+        }
+        return result;
+    }
 
 	@Transactional
 	@Override
-	public int deleteBoard(int id) {
+	public int deleteBoard(int id) throws IOException {
+		boardImageService.deleteAllByBoardId(id);
 		return boardMapper.deleteBoard(id);
 	}
 
 	@Transactional
 	@Override
-	public int updateBoard(Board board) {
-		return boardMapper.updateBoard(board);
-	}
+	public int updateBoard(Board board, List<MultipartFile> newImages, List<Integer> deleteImageIds) throws IOException {
+        // 1. 게시글 수정
+        int result = boardMapper.updateBoard(board);
+        if (result <= 0) {
+            throw new RuntimeException("게시글 수정 실패");
+        }
+
+        // 2. 이미지 삭제 처리
+        if (deleteImageIds != null && !deleteImageIds.isEmpty()) {
+            for (Integer imageId : deleteImageIds) {
+                boardImageService.deleteImage(imageId);
+            }
+        }
+
+        // 3. 새 이미지 추가
+        if (newImages != null && !newImages.isEmpty()) {
+            List<BoardImage> currentImages = board.getImages() != null ? 
+                board.getImages() : new ArrayList<>();
+                
+            for (MultipartFile image : newImages) {
+                if (!image.isEmpty()) {
+                    BoardImage savedImage = boardImageService.saveBoardImage(image, board.getId());
+                    currentImages.add(savedImage);
+                }
+            }
+            board.setImages(currentImages);
+        }
+
+        return result;
+    }
 
 	@Transactional
 	@Override
